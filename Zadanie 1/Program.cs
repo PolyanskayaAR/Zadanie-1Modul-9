70b39ff7d68518eaf700eabb10cdd4aa207d8519
Polyanskaya Anastaiya Ruslanovna <98982139+PolyanskayaAR@users.noreply.github.com>
2022-06-01 17:09:30 +0300

    Add files via upload

---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Method();
+                throw new Exception("Собственное исключение");
+            }
+
+            catch (Exception ex) when (ex.Message == "Собственное исключение")
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex) when (ex is FormatException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex) when (ex is ArgumentException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex) when (ex is IndexOutOfRangeException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex) when (ex is OverflowException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+
+            static void Method()
+            {
+                //Тело метода
+            }
+        }
+    }
+}
